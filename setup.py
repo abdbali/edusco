8026a0e9f310602d4f9eb20aa42f266e6318ea8e
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 09:24:37 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="edusco",
+    version="0.1.0",
+    author="Abdurrrahman Balı",
+    description="Anlamsal öğrenci cevabı değerlendirme sistemi ",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8",
+)
