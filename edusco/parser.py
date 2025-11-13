989ff7b4bb235cfb9d1961bacc6a5199ef87d14d
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 13:42:10 +0300

    Update parser.py

--- edusco/parser.py
+++ edusco/parser.py
@@ -2,5 +2,4 @@ from typing import List, Dict
 
 class POSParser:
     def parse(self, tokens: List[str]) -> List[Dict]:
-        # Basit örnek: tüm kelimeleri isim (NOUN) olarak işaretle
         return [{"token": t, "pos": "NOUN"} for t in tokens]
