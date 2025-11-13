b6cc50b7e7103bb2c6656210aecdc20d154d6898
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 11:08:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="edusco",
-    version="0.1.0",
-    author="Abdurrrahman Balı",
-    description="Anlamsal öğrenci cevabı değerlendirme sistemi ",
+    name='edusco',
+    version='0.1',
     packages=find_packages(),
-    install_requires=[],
-    python_requires=">=3.8",
+    install_requires=[
+        'pandas',
+        'ipywidgets',
+        'spellchecker',  # Eğer yazım düzeltme eklersen
+    ],
+    python_requires='>=3.8',
 )
