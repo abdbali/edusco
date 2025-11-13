fe1083e58bce51bf4695a9829b642dc724c312b4
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 14:16:10 +0300

    Update ontology.py

--- edusco/ontology.py
+++ edusco/ontology.py
@@ -1,9 +1,8 @@
 from typing import List, Dict
 
 class OntologyMatcher:
-
     def match(self, cevap_tokens: List[Dict], model_tokens: List[Dict]) -> List[str]:
-        cevap_set = set([t.get("root", t.get("word", "")) for t in cevap_tokens])
-        model_set = set([t.get("root", t.get("word", "")) for t in model_tokens])
+        cevap_set = set([t.get("root", t.get("token", "")) for t in cevap_tokens])
+        model_set = set([t.get("root", t.get("token", "")) for t in model_tokens])
         ortak = list(cevap_set.intersection(model_set))
-        return [o for o in ortak if o]
+        return ortak
