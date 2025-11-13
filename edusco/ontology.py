40b28d33bf82373f34367983d8c935480beb0fbd
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 13:21:18 +0300

    Update ontology.py

--- edusco/ontology.py
+++ edusco/ontology.py
@@ -1,7 +1,28 @@
 from typing import List, Dict
 
+# Basit eşanlamlı sözlük
+EQUIVALENCES = {
+    "arttırmak": ["çoğaltmak", "yükseltmek", "artırmak"],
+    "üretmek": ["oluşturmak", "yapmak", "üretilmek"],
+    "güneş": ["ışık"],
+    "bitki": ["bitkiler"],
+    "glikoz": ["şeker"],
+    "oksijen": ["o2"],
+    "fotosentez": ["fotosentez olayı"]
+}
+
 class OntologyMatcher:
     def match(self, cevap_tokens: List[Dict], model_tokens: List[Dict]) -> List[str]:
         cevap_set = set([t["root"] for t in cevap_tokens])
         model_set = set([t["root"] for t in model_tokens])
-        return list(cevap_set.intersection(model_set))
+        ortak = []
+
+        for m in model_set:
+            # Direkt eşleşme veya eşanlamlı
+            if m in cevap_set:
+                ortak.append(m)
+            elif m in EQUIVALENCES:
+                for eq in EQUIVALENCES[m]:
+                    if eq in cevap_set:
+                        ortak.append(m)
+        return ortak
