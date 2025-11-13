c4c825caa2eaf0e6ff743a07d6ac3f78aebfb85a
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 14:16:38 +0300

    Update model.py

--- edusco/model.py
+++ edusco/model.py
@@ -1,11 +1,8 @@
 class EduscoModel:
-
     def __init__(self, yanitlar):
-
         if isinstance(yanitlar, str):
             yanitlar = [yanitlar]
         self.yanitlar = yanitlar
 
     def get_all(self):
-
         return self.yanitlar
