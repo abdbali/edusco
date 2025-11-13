15e6e12745e6bc648ce7354b4bdf6ecc5e5f7027
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 13:36:50 +0300

    Update __init__.py

--- edusco/__init__.py
+++ edusco/__init__.py
@@ -5,3 +5,5 @@ from .tokenizer import Tokenizer
 from .parser import POSParser
 from .morphology import MorphologyAnalyzer
 from .ontology import OntologyMatcher
+from .extractor import Extractor
+from .pronoun_resolver import PronounResolver
