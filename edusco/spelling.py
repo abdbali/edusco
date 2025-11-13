5ab4e1310267246cb9a6fbb7c65ae1e7867c6723
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 13:17:59 +0300

    Create spelling.py

---
+++ edusco/spelling.py
@@ -0,0 +1,9 @@
+from spellchecker import SpellChecker
+
+class SpellingCorrector:
+    def __init__(self):
+        self.spell = SpellChecker(language='tr')
+
+    def correct(self, text: str) -> str:
+        kelimeler = text.strip().lower().split()
+        return " ".join([self.spell.correction(k) for k in kelimeler])
