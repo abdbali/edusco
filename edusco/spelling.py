274cfc805f51ddacfae45ffa8962d78c89c6964a
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 13:41:45 +0300

    Update spelling.py

--- edusco/spelling.py
+++ edusco/spelling.py
@@ -1,8 +1,10 @@
 from spellchecker import SpellChecker
+import os
 
 class SpellingCorrector:
     def __init__(self):
-        self.spell = SpellChecker(language='tr')
+        tr_dict_path = os.path.join(os.path.dirname(__file__), "resources", "tr.json")
+        self.spell = SpellChecker(language=None, local_dictionary=tr_dict_path)
 
     def correct(self, text: str) -> str:
         kelimeler = text.strip().lower().split()
