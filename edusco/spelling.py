6c1ba0a6578d9ffab75e0f24a84067e60b4901bf
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 15:01:41 +0300

    Update spelling.py

--- edusco/spelling.py
+++ edusco/spelling.py
@@ -3,9 +3,18 @@ import os
 
 class SpellingCorrector:
     def __init__(self):
+
         tr_dict_path = os.path.join(os.path.dirname(__file__), "resources", "tr.json")
         self.spell = SpellChecker(language=None, local_dictionary=tr_dict_path)
 
     def correct(self, text: str) -> str:
         kelimeler = text.strip().lower().split()
-        return " ".join([self.spell.correction(k) for k in kelimeler])
+
+        duzeltilmis = []
+        for k in kelimeler:
+            if k:  
+                c = self.spell.correction(k)
+                if c is None:
+                    c = k  
+                duzeltilmis.append(c)
+        return " ".join(duzeltilmis)
