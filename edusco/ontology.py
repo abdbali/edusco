2491eed440ebdd1e6d36939d52376cc228b9c6f0
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 14:12:29 +0300

    Update ontology.py

--- edusco/ontology.py
+++ edusco/ontology.py
@@ -1,25 +1,9 @@
 from typing import List, Dict
 
-EQUIVALENCES = {
-    "arttırmak": ["çoğaltmak", "yükseltmek", "artırmak"],
-    "üretmek": ["oluşturmak", "yapmak", "üretilmek"],
-    "güneş": ["ışık"],
-    "bitki": ["bitkiler"],
-    "glikoz": ["şeker"],
-    "oksijen": ["o2"],
-    "fotosentez": ["fotosentez olayı"]
-}
-
 class OntologyMatcher:
+
     def match(self, cevap_tokens: List[Dict], model_tokens: List[Dict]) -> List[str]:
-        cevap_set = set([t["root"] for t in cevap_tokens])
-        model_set = set([t["root"] for t in model_tokens])
-        ortak = []
-        for m in model_set:
-            if m in cevap_set:
-                ortak.append(m)
-            elif m in EQUIVALENCES:
-                for eq in EQUIVALENCES[m]:
-                    if eq in cevap_set:
-                        ortak.append(m)
-        return ortak
+        cevap_set = set([t.get("root", t.get("word", "")) for t in cevap_tokens])
+        model_set = set([t.get("root", t.get("word", "")) for t in model_tokens])
+        ortak = list(cevap_set.intersection(model_set))
+        return [o for o in ortak if o]
