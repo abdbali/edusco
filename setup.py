159d1ee7100c6e03183572575ffac0275de9ce4b
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-13 13:48:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
     install_requires=[
         "pyspellchecker"
     ],
-    python_requires='>=3.8'
+    package_data={
+        "edusco": ["resources/*.json"],  # <-- burası önemli
+    },
 )
